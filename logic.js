8d9556cc8ce3ef348aa50f39ec388f3af20caeea
Tanner W White <30422707+tannerwwhite@users.noreply.github.com>
2018-08-29 17:28:13 -0500

    content mostly complete
    still lots of styling to do and have trouble with img carousel

--- logic.js
+++ logic.js
@@ -1,8 +1,5 @@
-Function myMap(){
-    var mapOptions = {
-        center: new google.maps.LatLng(51.5, -0.12),
-        zoom:10,
-        mapTypeId: google.maps.MapTypeId.HYBRID
-    }
-    var map =new google.maps.Map(document.getElementById("map"),mapOptions);
-}
+$(document).ready(function(){
+  $('.carousel').slick({
+    setting-name: setting-value
+  });
+});
